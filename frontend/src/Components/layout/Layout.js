fd2cbe522f42c48caa226fdddc67b3e9bafa33f3
Aarya2702 <aryagode10@gmail.com>
2023-02-15 17:00:35 +0530

    Navbar and Login Fix

--- frontend/src/Components/layout/Layout.js
+++ frontend/src/Components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom';
 import Loader from '../../helper/Loader';
-import Navbar from '../Header/Navbar';
+import Navbar from '../Navbar/NavBar.jsx';
 import Footer from "../Footer/Footer"
 
 const Layout = () => {
