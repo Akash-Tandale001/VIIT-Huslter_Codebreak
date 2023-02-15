4a108961988e4d818f8da64a4e53657790d65f82
sid120 <siddharthrakshe12@gmail.com>
2023-02-16 05:26:08 +0530

    updated aboutus

--- frontend/src/Components/Navbar/AboutUs.jsx
+++ frontend/src/Components/Navbar/AboutUs.jsx
@@ -9,22 +9,28 @@ function AboutUs() {
             About Us
           </h1>
           <p className="text-ggimray-800 text-lg leading-relaxed mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            maximus auctor velit, eu interdum augue rhoncus vel. Vestibulum
-            lobortis euismod ex, a lacinia eros ultricies vel. In eget magna sed
-            felis consectetur efficitur id id augue.
+            An Indian automaker called Ashok Leyland is focused on producing
+            trucks, buses, and other commercial and military vehicles. Chennai,
+            India is home to the company's headquarters, which was
+            established in 1948.
           </p>
           <p className="text-gray-800 text-lg leading-relaxed mb-4">
-            Aenean euismod feugiat risus ac volutpat. Fusce aliquet justo eu
-            ultrices dignissim. Etiam nec euismod ante. Nulla facilisi. Morbi
-            vel magna ut libero vehicula commodo. Integer tincidunt sapien at
-            malesuada commodo.
+            Ashok Leyland is one of the largest manufacturers of commercial
+            vehicles in India and has a significant global presence in countries
+            such as the United Kingdom, Sri Lanka, and the Middle East. The
+            company has a wide range of products that cater to various segments,
+            including long-haul transportation, construction, mining, and
+            public transportation.
           </p>
           <p className="text-gray-800 text-lg leading-relaxed mb-4">
-            Nulla facilisi. Morbi vel magna ut libero vehicula commodo. Integer
-            tincidunt sapien at malesuada commodo. Aenean euismod feugiat risus
-            ac volutpat. Fusce aliquet justo eu ultrices dignissim. Etiam nec
-            euismod ante.
+            Apart from commercial vehicles, Ashok Leyland also offers related
+            products and services such as aftermarket support, fleet management
+            solutions, and customized vehicle modifications. The company is
+            known for its focus on innovation, technology, and sustainability,
+            with a strong emphasis on research and development.Overall, Ashok
+            Leyland is a leading player in the Indian commercial vehicle
+            industry and is recognized for its high-quality products and
+            services, innovative solutions, and commitment to sustainability.
           </p>
           <div className="flex justify-center mt-8">
             <a
@@ -35,7 +41,6 @@ function AboutUs() {
             </a>
           </div>
         </div>
-        
       </div>
     </>
   );
