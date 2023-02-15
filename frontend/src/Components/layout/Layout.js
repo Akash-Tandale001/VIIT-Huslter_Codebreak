e56957be6e6ba8a2e053f1165eaf6117c4b073d4
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-02-15 17:13:53 +0530

    bug fix

--- frontend/src/Components/layout/Layout.js
+++ frontend/src/Components/layout/Layout.js
@@ -1,20 +1,19 @@
-import React, { Suspense } from 'react'
-import { Outlet } from 'react-router-dom';
-import Loader from '../../helper/Loader';
-import Navbar from '../Header/Navbar';
-import Footer from "../Footer/Footer"
+import React, { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import Loader from "../../helper/Loader";
+import NavBar from "../../Components/Navbar/NavBar";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
     <>
-    <Navbar/>
-    <Suspense fallback={<Loader />}>
-    <Outlet></Outlet>
-  </Suspense>
-  <Footer/>
-    
+      <NavBar />
+      <Suspense fallback={<Loader />}>
+        <Outlet></Outlet>
+      </Suspense>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import NavBar from '../../Components/Navbar/NavBar'
+
 import HeroCarausel from '../../Components/HeroSection/HeroCarausel'
-import Login from '../SignIn/pages/Login'
-import Signup from '../SignIn/pages/SignUp'
 
 const Home = () => {
-  console.log("wtf")
   return (
    
     <>
-     <div>
-      <NavBar/>
-    </div>
     <HeroCarausel/>
-    <Login/>
-    <Signup/>
     </>
   )
 }
