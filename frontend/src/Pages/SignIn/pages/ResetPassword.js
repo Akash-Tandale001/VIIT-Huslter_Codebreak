1f41768044c95bd2891e5e3d23096894b77e641e
Aarya2702 <aryagode10@gmail.com>
2023-02-15 19:58:56 +0530

    ResetP Update

--- frontend/src/Pages/SignIn/pages/ResetPassword.js
+++ frontend/src/Pages/SignIn/pages/ResetPassword.js
@@ -1,20 +1,18 @@
-import Header from "../Components/Header";
-import ResetPassword from "../Components/ResetPassword";
+import Header from "../Components/Header"
+import ResetPassword from "../Components/ResetPassword"
 
 export default function ResetPasswordPage(){
     return(
         <>
         <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div className="max-w-md w-full space-y-8">
-            <Header
-              heading="Reset Password"
-              paragraph="Already have an account? "
-              linkName="Login"
-              linkUrl="/Login"
-            />
+    <Header
+                heading="Reset Password"
+                />
             <ResetPassword/>
-            </div>
-            </div>
+    </div>
+    </div>
+             
         </>
     )
 }
