60824c529e6878cc878686850ee40321e202bd5a
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-02-15 16:43:27 +0530

    updated

--- frontend/src/Components/layout/Layout.js
+++ frontend/src/Components/layout/Layout.js
@@ -1,18 +1,17 @@
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom';
 import Loader from '../../helper/Loader';
-import Navbar from '../Header/Navbar';
 import Footer from "../Footer/Footer"
+import NavBar from '../Navbar/NavBar'
 
 const Layout = () => {
   return (
     <>
-    <Navbar/>
+    <NavBar/>
     <Suspense fallback={<Loader />}>
     <Outlet></Outlet>
-  </Suspense>
-  <Footer/>
-    
+    </Suspense>
+    <Footer/>
     </>
   )
 }
