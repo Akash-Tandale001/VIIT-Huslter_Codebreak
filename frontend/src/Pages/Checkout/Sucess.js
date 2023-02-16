bb73d0260371b35d183e096ea09229b48f649d05
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-02-16 10:59:39 +0530

    payment done

---
+++ frontend/src/Pages/Checkout/Sucess.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sucess = () => {
+  return (
+    <div className="flex justify-center mb-10">
+      <img src='http://www.shikharclasses.in/wp-content/uploads/2020/04/PAYMENT-SUCCESS.png'/>
+    </div>
+  )
+}
+
+export default Sucess
